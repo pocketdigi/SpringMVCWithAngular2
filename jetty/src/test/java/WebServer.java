e9a00c83299f2c0701a2dfff7d14144faf9115aa
Exception <webmaster@pocketdigi.com>
2016-11-01 16:37:38 +0800

    Jetty,日志

---
+++ jetty/src/test/java/WebServer.java
@@ -0,0 +1,26 @@
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Jetty服务器
+ */
+public class WebServer {
+    public static void main(String[] args) {
+        Server server = new Server(8080);
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setDescriptor("./web/src/main/webapp/WEB-INF/web.xml");
+        context.setResourceBase("./web/src/main/webapp");
+        //解决静态资源缓存后再ide里面不能修改问题
+        context.setDefaultsDescriptor("./jetty/src/test/resources/webdefault.xml");
+        context.setParentLoaderPriority(true);
+        server.setHandler(context);
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
